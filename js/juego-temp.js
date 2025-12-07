6252abddbec099fcb51f262a0f53c14f8c071823
Matias Llanos <llanosmatias97@gmail.com>
2025-12-07 15:28:05 -0300

    Aplicar responsive design y ajustes visuales finales

---
+++ js/juego-temp.js
@@ -0,0 +1,10 @@
+'use strict';
+(function() {
+var TAMANO_TABLERO = 8;
+var TOTAL_MINAS = 10;
+var tablero = [];
+function inicializar() {
+    console.log('Juego iniciado');
+}
+document.addEventListener('DOMContentLoaded', inicializar);
+})();
